c570c36a06e54fc36b700dfcd494bd7ccefd0a68
moacirjres20 <jose.fonseca@aluno.faculdadeimpacta.com.br>
2021-02-25 23:32:43 +0000

    task 2

--- src/main/java/br/com/impacta/lab/controller/HelloWorldController.java
+++ src/main/java/br/com/impacta/lab/controller/HelloWorldController.java
@@ -19,9 +19,8 @@ public class HelloWorldController {
 		//Considerando a dica acima, implemente seu código para que o programa retorne a 
 		//palavra Hello world concatenada com o nome.
 		//Exmeplo de resposta: Hello world felipe !
-		
-		
-		return ResponseEntity.ok("Hello world !");
+			
+		return ResponseEntity.ok("Hello world " + nome + " !");
 	}
 	
 }
